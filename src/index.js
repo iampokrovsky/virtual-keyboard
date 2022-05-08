4b8f833e87b8e3932049153520ed61e24bc3d425
Andrey Pokrovsky <iampokrovsky@gmail.com>
2022-05-08 11:48:10 +0300

    feat: add template layout

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import '@/index.html';
 import '@styles/style.scss';
+import 'simplebar';
+import 'simplebar/dist/simplebar.css';
+
+console.log(window.navigator);
+document.execCommand('defaultParagraphSeparator', false, 'span');
+
+setTimeout(() => {
+  document.querySelector('.textarea__content').focus();
+}, 1000);
